4fa0ef25e5123182d0f35369c5cf9b9ebe3346d3
Raksha Thakkar <raxa1989@gmail.com>
2021-02-19 00:16:37 +0530

    Add ItemGroup

---
+++ Command.Entity1/ItemGroup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Command.Entity1
+{
+    [Table("ItemGroup", Schema = "po")]
+    public partial class ItemGroup
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [StringLength(64)]
+        public string Name { get; set; }
+        [StringLength(20)]
+        public string CreatedBy { get; set; }
+
+        [Column(TypeName = "datetime")]
+        public DateTime CreatedDate { get; set; }
+
+        [StringLength(50)]
+        public string ModifiedBy { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime ModifiedDate
+        {
+            get
+            {
+                return this.dateCreated.HasValue
+                   ? this.dateCreated.Value
+                   : DateTime.Now;
+            }
+
+            set { this.dateCreated = value; }
+        }
+
+        private DateTime? dateCreated = null;
+        [StringLength(1)]
+        public string Status { get; set; }
+    }
+}
